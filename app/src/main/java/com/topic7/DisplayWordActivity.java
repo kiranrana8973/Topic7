c646a582c6e34a3222c8731c7b2703850ec37482
kiran <kiranrana8973@gmail.com>
2019-05-01 22:28:28 +0545

    Finishded sqllite

--- app/src/main/java/com/topic7/DisplayWordActivity.java
+++ app/src/main/java/com/topic7/DisplayWordActivity.java
@@ -3,6 +3,7 @@ package com.topic7;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,8 +22,7 @@ public class DisplayWordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_word);
 
         lstWord = findViewById(R.id.lstWords);
-       LoadWord();
-
+        LoadWord();
     }
 
     private void LoadWord() {
@@ -30,11 +30,20 @@ public class DisplayWordActivity extends AppCompatActivity {
         final SQLiteDatabase sqLiteDatabase = myHelper.getWritableDatabase();
 
         List<Word> wordList = new ArrayList<>();
-        wordList = myHelper.GetAllWords(sqLiteDatabase);
+        //wordList = myHelper.GetAllWords(sqLiteDatabase);
+        wordList = myHelper.GetWordByName("Hello",sqLiteDatabase);
 
-        HashMap<String,String> hashMap = new HashMap<>();
+        HashMap<String, String> hashMap = new HashMap<>();
 
+        for (int i = 0; i < wordList.size(); i++) {
 
-
+            hashMap.put(wordList.get(i).getWord(), wordList.get(i).getMeaning());
+        }
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                new ArrayList<String>(hashMap.keySet())
+        );
+        lstWord.setAdapter(stringArrayAdapter);
     }
 }
